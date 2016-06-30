7f3d1d9ae8bc611d9d00d95f64ad67ec759a4058
kekuso <kekuso808@gmail.com>
2016-06-29 15:45:30 -1000

    Attempting to check for duplicate usernames.

--- server.js
+++ server.js
@@ -9,45 +9,61 @@ var input = process.stdin;
 // Connection listener
 var server = net.createServer(function (socket) {
   clients.push(socket);
-  console.log("Enter a username: ");
+  socket.write("Enter a username: ");
   var socketAddress = socket.address().address;
   var socketPort = socket.remotePort;
   var broadcast;
+  var duplicate = false;
   console.log("CONNECTED: " + socketAddress + ":" + socketPort);
 
   socket.on('data', function(data) {
     // populate array of usernames
     if(userArray.length < clients.length) {
       if(data.toString() === '[ADMIN]\n') {
-        console.log("YOU ARE NOT WORTHY TO BE [ADMIN]");
-        console.log("YOU ARE DEEMED LOSERX");
-        socket.write("You fool.");
-        userArray[count] = 'LOSER' + count;
+        console.log("Imposter detected.");
+        socket.write("[ADMIN] You are an imposter. Leave or forever be known as one.");
+        userArray[count] = 'IMPOSTER' + count;
         count++;
       }
       else {
-        userArray[count] = data;
-        count++;
-        //console.log("Added username: " + data + " to array index " + count);
+        for(var k = 0; k < userArray.length; k++) {
+          if(data.toString() === userArray[k]) {
+            socket.write("That username is already taken. You will be named John Doe X.");
+            userArray[count] = 'John Doe ' + count;
+            count++;
+            duplicate = true;
+          }
+          else {
+            duplicate = false;
+          }
+        }
+        if(!duplicate) {
+          userArray[count] = data;
+          count++;
+          socket.write("Welcome " + data);
+          duplicate = false;
+        }
       }
     }
 
-    // display client's message on server
-    var index = clients.indexOf(socket);
-    console.log(('SERVER BCAST FROM ' + socketAddress + ":" + socket.remotePort + " " + userArray[index] + ": " + data).replace(/(\r\n|\n|\r)/gm,""));
+    else {
+      // display client's message on server
+      var index = clients.indexOf(socket);
+      console.log(('SERVER BCAST FROM ' + socketAddress + ":" + socket.remotePort + " " + userArray[index] + ": " + data).replace(/(\r\n|\n|\r)/gm,""));
 
-    // determine which client sent the message
-    for(var i = 0; i < clients.length; i++) {
-      if(i === clients.indexOf(socket)) {
-        socketAddress = clients[i].remoteAddress;
-        socketPort = clients[i].remotePort;
-        broadcast = (socketAddress + ":" + socketPort + " " + userArray[i] + ": " + data).replace(/(\r\n|\n|\r)/gm,"");
+      // determine which client sent the message
+      for(var i = 0; i < clients.length; i++) {
+        if(i === clients.indexOf(socket)) {
+          socketAddress = clients[i].remoteAddress;
+          socketPort = clients[i].remotePort;
+          broadcast = (socketAddress + ":" + socketPort + " " + userArray[i] + ": " + data).replace(/(\r\n|\n|\r)/gm,"");
+        }
       }
-    }
 
-    // display client's message to all clients
-    for(var j = 0; j < clients.length; j++) {
-      clients[j].write(broadcast);
+      // display client's message to all clients
+      for(var j = 0; j < clients.length; j++) {
+        clients[j].write(broadcast);
+      }
     }
   });
 
@@ -60,7 +76,7 @@ var server = net.createServer(function (socket) {
   socket.on('end', function (socket) {
     console.log('CLOSED: ' + socketAddress + ":" + socketPort);
     var index = clients.indexOf(socket);
-    clients.splice(clients.indexOf(socket), 1);
+    clients.splice(index, 1);
     userArray.splice(index, 1);
     count--;
   });
