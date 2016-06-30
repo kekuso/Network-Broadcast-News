3ad41420286153f1f644df7b064f5f8139579e0e
kekuso <kekuso808@gmail.com>
2016-06-29 14:01:13 -1000

    Implemented server broadcasting to all clients.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var CONFIG = require('./config');
 var clients = [];
 var count = 0;
 var userArray = [];
+var input = process.stdin;
+
 // Connection listener
 var server = net.createServer(function (socket) {
   clients.push(socket);
@@ -14,14 +16,18 @@ var server = net.createServer(function (socket) {
   console.log("CONNECTED: " + socketAddress + ":" + socketPort);
 
   socket.on('data', function(data) {
+    // populate array of usernames
     if(userArray.length < clients.length || userArray.length === 0) {
       userArray[count] = data;
       count++;
       console.log("Added username: " + data + " to array index " + count);
     }
+
+    // display client's message on server
     var index = clients.indexOf(socket);
     console.log(('SERVER BCAST FROM ' + socketAddress + ":" + socket.remotePort + " " + userArray[index] + ": " + data).replace(/(\r\n|\n|\r)/gm,""));
 
+    // determine which client sent the message
     for(var i = 0; i < clients.length; i++) {
       if(i === clients.indexOf(socket)) {
         socketAddress = clients[i].remoteAddress;
@@ -30,11 +36,18 @@ var server = net.createServer(function (socket) {
       }
     }
 
+    // display client's message to all clients
     for(var j = 0; j < clients.length; j++) {
       clients[j].write(broadcast);
     }
   });
 
+  // broadcast server's message to all clients
+  input.on('data', function (data) {
+    socket.write("[ADMIN]: " + data);
+  });
+
+  // decrement arrays when a client disconnects
   socket.on('end', function (socket) {
     console.log('CLOSED: ' + socketAddress + ":" + socketPort);
     var index = clients.indexOf(socket);
@@ -42,10 +55,6 @@ var server = net.createServer(function (socket) {
     userArray.splice(index, 1);
     count--;
   });
-
-  // function addUser (name) {
-  //   console.log("Enter a username: ");
-  // }
 });
 
 server.listen(CONFIG.PORT, function () {
